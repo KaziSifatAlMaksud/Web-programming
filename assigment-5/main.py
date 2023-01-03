30df86da572c2dbf664d05e110a831fa890804b0
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2023-01-04 00:14:37 +0600

    assigment5

--- assigment-5/main.py
+++ assigment-5/main.py
@@ -41,29 +41,74 @@ def register():
             f_phone_l = False
 
         # returen valuo
-        if :
-            return " User Already Exist "
-        if f_pass_m == True:
+        if f_user == True  and f_pass_m == False  and f_len == False and f_email == False and f_phone_l == False:
+            return "User Already Exist "
+        if f_user == False  and f_pass_m == True  and f_len == False and f_email == False and f_phone_l == False:
             return "Password is not Match"
-        if f_len == True:
+        if f_user == False  and f_pass_m == False  and f_len == True and f_email == False and f_phone_l == False:
             return "Enter more den 6 character !!"
-        if f_email == True:
+        if f_user == False  and f_pass_m == False  and f_len == False and f_email == True and f_phone_l == False:
             return "Email Already Exist "
-        if f_phone_l == True:
-            return "Phone number is not have 11 digit"
-
-        if f_user == True  and caseB == False  and caseC == False and caseD == False and caseBB == False:
-            return "Email Already Exist "
-        if caseA == False and caseB == False  and caseC == False and caseD == False and caseBB == False:
-            return "Email Already Exist "
-        if caseA == False and caseB == False  and caseC == False and caseD == False and caseBB == False:
+        if f_user == False  and f_pass_m == False  and f_len == False and f_email == False and f_phone_l == True:
+            return "Mobile Number Already Exist "
+        if f_user == True  and f_pass_m == True  and f_len == False and f_email == False and f_phone_l == False:
+            return "User Already Exist <br/> Password is not Match"
+        if f_user == True  and f_pass_m == False  and f_len == True and f_email == False and f_phone_l == False:
+            return "User Already Exist <br/>Enter more den 6 character !!"
+        if f_user == True  and f_pass_m == False  and f_len == False and f_email == True and f_phone_l == False:
+            return "User Already Exist <br/>Email Already Exist "
+        if f_user == True  and f_pass_m == False  and f_len == False and f_email == False and f_phone_l == True:
+            return "User Already Exist <br/> Mobile Number Already Exist "
+        if f_user == True  and f_pass_m == False  and f_len == False and f_email == True and f_phone_l == True:
+            return "User Already Exist  <br/> Email Already Exist <br/> Mobile Number Already Exist "
+        if f_user == True  and f_pass_m == False  and f_len == True and f_email == False and f_phone_l == True:
+            return "User Already Exist <br/> Enter more den 6 character !! <br/> Mobile Number Already Exist  "
+        if f_user == True  and f_pass_m == True  and f_len == False and f_email == False and f_phone_l == True:
+            return "User Already Exist <br/> Password is not Match <br/> Mobile Number Already Exist"
+        if f_user == False  and f_pass_m == True  and f_len == False and f_email == False and f_phone_l == True:
+            return "Password is not Match <br/> Mobile Number Already Exist"
+        if f_user == False  and f_pass_m == False  and f_len == True and f_email == False and f_phone_l == True:
+            return "Enter more den 6 character !! <br/> Mobile Number Already Exist"
+        if f_user == False  and f_pass_m == False  and f_len == False and f_email == True and f_phone_l == True:
+            return "Email Already Exist <br/> Mobile Number Already Exist"
+        if f_user == False  and f_pass_m == False  and f_len == True and f_email == True and f_phone_l == True:
+            return "Enter more den 6 character !!<br/>Email Already Exist <br/> Mobile Number Already Exist"
+        if f_user == True  and f_pass_m == True  and f_len == True and f_email == True and f_phone_l == True:
+            return " User Already Exist <br/> Password is not Match <br> Enter more den 6 character !! <br/>Email Already Exist  <br/>  Mobile Number Already Exist "
+        if f_user == True and f_pass_m == True and f_len == True and f_email == False and f_phone_l == True:
+            return " User Already Exist<br/>Password is not Match<br> Enter more den 6 character !!<br/> Mobile Number Already Exist"
+        if f_user == False and f_pass_m == False and f_len == True and f_email == True and f_phone_l == False:
+            return " Enter more den 6 character !!<br/>Email Already Exist "
+        if f_user == False and f_pass_m == True and f_len == False and f_email == False and f_phone_l == True:
+            return " Password is not Match <br/> Mobile Number Already"
+        if f_user == False and f_pass_m == True and f_len == False and f_email == True and f_phone_l == False:
+            return " Password is not Match <br/> Email Already Exist"
+        if f_user == False and f_pass_m == True and f_len == False and f_email == True and f_phone_l == True:
+            return " Password is not Match <br/> Email Already Exist<br/> Mobile Number Already"
+        if f_user == False and f_pass_m == True and f_len == True and f_email == True and f_phone_l == False:
+            return " Password is not Match <br/> Email Already Exist"
+        if f_user == False and f_pass_m == True and f_len == True and f_email == True and f_phone_l == False:
+            return " Password is not Match <br> Enter more den 6 character !! <br/> Email Already Exist"
+        if f_user == True and f_pass_m == True and f_len == True and f_email == True and f_phone_l == False:
+            return "User Already Exist <br/> Password is not Match <br/> Enter more den 6 character !! <br/> Email Already Exist"
+        if f_user == True and f_pass_m == True and f_len == True and f_email == False and f_phone_l == False:
+            return " User Already Exist <br/>Password is not Match <br> Enter more den 6 character !!  "
+        if f_user == True and f_pass_m == True and f_len == False and f_email == True and f_phone_l == True:
+            return " User Already Exist <br/>Password is not Match <br> Email Already Exist<br> Mobile Number Already "
+        if f_user == True and f_pass_m == True and f_len == False and f_email == True and f_phone_l == False:
+            return " User Already Exist <br/>Password is not Match <br> Email Already Exist"
+        if f_user == True and f_pass_m == False and f_len ==True and f_email == True and f_phone_l == False:
+            return " User Already Exist <br> Enter more den 6 character !! <br> Email Already Exist "
+        if f_user == True and f_pass_m == False and f_len ==True and f_email == True and f_phone_l == True:
+            return " User Already Exist <br> Enter more den 6 character !! <br> Email Already Exist <br> Mobile Number Already "
+        if f_user == False  and f_pass_m == False  and f_len == False and f_email == False and f_phone_l == False:
             db.users.insert_one(newUser)
             return "Registration Successsfull"
 
     return render_template("register.html", **locals())
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5009)
+    app.run(host='127.0.0.1', port=50001)
     #serve(app, host='127.0.0.1', port=5002)
     #serve(app, host='0.0.0.0', port=80)
 
